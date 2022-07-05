e940453cdbf67adbcfe5eb493a45adce50787890
Vitaliy Zhuravel <104020905+ikarkam76@users.noreply.github.com>
2022-07-05 12:13:08 +0300

    finish render function

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -5,24 +5,42 @@ const countryInfo = document.querySelector('.country-info');
 
 export function fetchCountries(evt) {
   const name = evt.target.value.trim();
-  fetch(
-    `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,languages,flags`
-  )
-    .then(response => {
-      return response.json();
-    })
-    .then(countries => renderCountriesList(countries))
-    .catch(error => Notify.failure('Oops, there is no country with that name'));
+  if (!name) {
+    countryList.innerHTML = '';
+    countryInfo.innerHTML = '';
+  } else {
+    fetch(
+      `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,languages,flags`
+    )
+      .then(response => {
+        return response.json();
+      })
+      .then(countries => renderCountriesList(countries))
+      .catch(error => Notify.failure('Oops, there is no country with that name'));
+  }
 }
 
 function renderCountriesList(countries) {
-  console.log(countries.length);
   if (countries.length <= 0) {
-      return
-  } else { 
+    return;
+  } else {
     if (countries.length > 10) {
+      countryList.innerHTML = '';
+      countryInfo.innerHTML = '';
       Notify.info('Too many matches found. Please enter a more specific name.');
     } else {
+      if (countries.length === 1) {
+        countryList.innerHTML = '';
+        const htmlString = `<div class="country__box">
+            <img src="${countries[0].flags.svg}" alt="flag of country" class="country__big-flag">
+            <p class="country__big-name">${countries[0].name.official}</p>
+            </div>
+            <p class="country__info">Capital:  ${countries[0].capital}</p>
+            <p class="country__info">Population:  ${countries[0].population}</p>
+            <p class="country__info">Languages:  ${Object.values(countries[0].languages).join(',')}</p>`;
+        countryInfo.innerHTML = htmlString;
+      } else {
+        countryInfo.innerHTML = '';
         const markup = countries
           .map(country => {
             return `<li class="country__item">
@@ -32,6 +50,8 @@ function renderCountriesList(countries) {
           })
           .join('');
         countryList.innerHTML = markup;
+      }
     }
   }
-}
+};
+
