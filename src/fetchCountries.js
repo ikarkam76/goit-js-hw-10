88465d0baeeccebdd1870119a37bb8ed61b79ffc
Vitaliy Zhuravel <104020905+ikarkam76@users.noreply.github.com>
2022-07-05 14:02:08 +0300

    add styles on body

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -35,17 +35,17 @@ function renderCountriesList(countries) {
             <img src="${countries[0].flags.svg}" alt="flag of country" class="country__big-flag">
             <p class="country__big-name">${countries[0].name.official}</p>
             </div>
-            <p class="country__info">Capital:  ${countries[0].capital}</p>
-            <p class="country__info">Population:  ${countries[0].population}</p>
-            <p class="country__info">Languages:  ${Object.values(countries[0].languages).join(',')}</p>`;
+            <p class="country__info"><span class="info__name">Capital:  </span>${countries[0].capital}</p>
+            <p class="country__info"><span class="info__name">Population:  </span>${countries[0].population}</p>
+            <p class="country__info"><span class="info__name">Languages:  </span>${Object.values(countries[0].languages).join(', ')}</p>`;
         countryInfo.innerHTML = htmlString;
       } else {
         countryInfo.innerHTML = '';
-        const markup = countries
-          .map(country => {
+          const markup = countries
+            .map(country => {
             return `<li class="country__item">
               <img src="${country.flags.svg}" alt="flag of country" class="country__flag">
-              <p class="country__name">${country.name.official}</p>
+              <p class="country__name">${country.name.common}</p>
             </li>`;
           })
           .join('');
