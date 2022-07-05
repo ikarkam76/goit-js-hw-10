1e7e4e0d35ec6daaba3045c5007800ec9a291d04
Vitaliy Zhuravel <104020905+ikarkam76@users.noreply.github.com>
2022-07-05 09:48:09 +0300

    add filter on render

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,25 +1,37 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
 const countryList = document.querySelector('.country-list');
 const countryInfo = document.querySelector('.country-info');
 
-export function fetchCountries(name) {
+export function fetchCountries(evt) {
+  const name = evt.target.value.trim();
   fetch(
-    `https://restcountries.com/v3.1/name/${name.target.value}?fields=name,capital,population,languages,flags`
+    `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,languages,flags`
   )
     .then(response => {
       return response.json();
     })
     .then(countries => renderCountriesList(countries))
-    .catch(error => alert("error"));
+    .catch(error => Notify.failure('Oops, there is no country with that name'));
 }
 
 function renderCountriesList(countries) {
-  const markup = countries
-    .map(country => {
-      return `<li class="country__item">
-          <img src="${country.flags.svg}" alt="flag of country" class="country__flag">
-          <p class="country__name">${country.name.official}</p>
-        </li>`;
-    })
-    .join('');
-  countryList.innerHTML = markup;
+  console.log(countries.length);
+  if (countries.length <= 0) {
+      return
+  } else { 
+    if (countries.length > 10) {
+      Notify.info('Too many matches found. Please enter a more specific name.');
+    } else {
+        const markup = countries
+          .map(country => {
+            return `<li class="country__item">
+              <img src="${country.flags.svg}" alt="flag of country" class="country__flag">
+              <p class="country__name">${country.name.official}</p>
+            </li>`;
+          })
+          .join('');
+        countryList.innerHTML = markup;
+    }
+  }
 }
